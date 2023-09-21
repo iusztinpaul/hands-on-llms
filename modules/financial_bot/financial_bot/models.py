abe98475a3bbd26ed99a244afabe20f85c0d0c6c
Joywalker <alexandrurazvant@gmail.com>
2023-09-21 23:36:15 +0300

    BOT1 chore: info log when downloading model from MR

--- modules/financial_bot/financial_bot/models.py
+++ modules/financial_bot/financial_bot/models.py
@@ -100,6 +100,9 @@ def build_qlora_model(
     if peft_pretrained_model_name_or_path:
         is_model_name = not os.path.isdir(peft_pretrained_model_name_or_path)
         if is_model_name:
+            logger.info(
+                f"Downloading {peft_pretrained_model_name_or_path} from CometML Model Registry:"
+            )
             peft_pretrained_model_name_or_path = download_from_model_registry(
                 model_id=peft_pretrained_model_name_or_path,
                 cache_dir=cache_dir,
