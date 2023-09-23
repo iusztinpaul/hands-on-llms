40e3daf96892d534c6065be8fe1198ad8cd475d6
Joywalker <alexandrurazvant@gmail.com>
2023-09-23 15:08:36 +0300

    bot1 feat: added run_chain test script

--- modules/financial_bot/tools/run_chain.py
+++ modules/financial_bot/tools/run_chain.py
@@ -3,38 +3,17 @@ import fire
 
 dotenv.load_dotenv()
 
-from financial_bot import chains
-from financial_bot.embeddings import EmbeddingModelSingleton
-from financial_bot.models import build_huggingface_pipeline
-from financial_bot.qdrant import build_qdrant_client
-from langchain.chains import SequentialChain
+from financial_bot.langchain_bot import FinancialBot
 
 
 def main():
-    qdrant = build_qdrant_client()
-    embeddings_model = EmbeddingModelSingleton()
+    bot = FinancialBot()
+    input_payload = {
+        "about_me": "I'm a student and I have some money that I want to invest.",
+        "question": "Should I consider investing in stocks from the Tech Sector?",
+    }
 
-    quest_retrieval_chain = chains.PreparePromptChain(
-        embedding_model=embeddings_model, vector_store=qdrant
-    )
-
-    llm_model = build_huggingface_pipeline()
-
-    llm_chain = chains.FinancialBotQAChain(hf_pipeline=llm_model)
-    seq_chain = SequentialChain(
-        chains=[quest_retrieval_chain, llm_chain],
-        input_variables=["about_me", "question"],
-        output_variables=["response"],
-        verbose=True,
-    )
-
-    response = seq_chain.run(
-        {
-            "about_me": "I'm a 27 years old IT programmer.",
-            "question": "Is it a good time to invest in Microsoft stock?",
-        }
-    )
-
+    response = bot.answer(**input_payload)
     print(response)
 
 
