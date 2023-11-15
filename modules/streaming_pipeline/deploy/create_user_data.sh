226450d7b50866869c55ba7bb222274a20b0fd6f
iusztinpaul <p.e.iusztin@gmail.com>
2023-11-15 08:46:48 +0200

    fix: Missing AWS profile

--- modules/streaming_pipeline/deploy/create_user_data.sh
+++ modules/streaming_pipeline/deploy/create_user_data.sh
@@ -8,7 +8,7 @@ else
 fi
 
 # Query the ECR registry URI.
-export ECR_REGISTRY_URI=$(aws ecr describe-repositories --repository-names ${AWS_ECR_REPO_NAME} --query "repositories[?repositoryName==\`${AWS_ECR_REPO_NAME}\`].repositoryUri" --output text --profile $AWS_PROFILE --region $AWS_REGION)
+export ECR_REGISTRY_URI=$(aws ecr describe-repositories --repository-names ${AWS_ECR_REPO_NAME} --query "repositories[?repositoryName==\`${AWS_ECR_REPO_NAME}\`].repositoryUri" --output text --region $AWS_REGION)
 if [ -z "$ECR_REGISTRY_URI" ]; then
     echo "ECR_REGISTRY_URI is not set. Most probably because the AWS_ECR_REPO_NAME=${AWS_ECR_REPO_NAME} ECR repository does not exist. Exiting script."
     exit 1
